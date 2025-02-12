6da1b729b21174b37b62bd38b56113cbaafe7fab
felipefonteslisboa <felipefonteslisboa@gmail.com>
2025-02-12 13:34:36 -0300

    Barra de progressso

---
+++ script.js
@@ -0,0 +1,18 @@
+function iniciarBarraDeProgresso(duracao) {
+    let tempoRestante = duracao; 
+    const barra = document.getElementById("minhaBarra");
+    const contagem = document.getElementById("contagem");
+
+    
+    const intervalo = setInterval(frame, 50);
+    function frame() {
+        if (tempoRestante <= 1) {
+            clearInterval(intervalo);
+        } else {
+            tempoRestante -= 0.1; 
+            barra.style.width = (tempoRestante / duracao) * 100 + "%"; 
+            contagem.textContent = tempoRestante.toFixed() + "s"; 
+        }
+    }
+}
+iniciarBarraDeProgresso(5);
